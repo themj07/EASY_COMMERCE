168e4c6b13ee40dd97387f86b84041ea7badd89e
Michel Amou <amoujeanmichel17@gmail.com>
2023-02-10 21:38:58 +0000

    suite

--- FINAL/finalproject/finalproject/settings.py
+++ FINAL/finalproject/finalproject/settings.py
@@ -117,24 +117,27 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 
 
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'dbeasycommerce',
-       'USER': 'postgres',
-       'PASSWORD': 'dieuestgrand',
-       'HOST': '127.0.0.1',
-       'PORT': '5432',
-   }
-}
+
 
 
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'dbeasycommerce',
+#        'USER': 'postgres',
+#        'PASSWORD': 'dieuestgrand',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
