aae5d2a588d22c5130b10e71922b79e2c5e42ef9
Michel Amou <amoujeanmichel17@gmail.com>
2023-02-10 19:00:06 +0000

    suite

--- FINAL/finalproject/finalproject/settings.py
+++ FINAL/finalproject/finalproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 
 import django
@@ -170,6 +173,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
